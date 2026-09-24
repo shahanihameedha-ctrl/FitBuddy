ad45e6fb2ab832cca0c2a51a9dd2ebd249038313
shahanihameedha-ctrl <shahanihameedha@gmail.com>
2026-09-24 14:16:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// FitBuddy JavaScript Code
+console.log("FitBuddy App Loaded Successfully!");
+
+function startWorkout() {
+    alert("Workout Started! Keep moving!");
+}
+
+// Simple Fitness Tracker Logic
+let dailySteps = 0;
+
+function addSteps(steps) {
+    dailySteps += steps;
+    console.log(`Total steps today: ${dailySteps}`);
+}
